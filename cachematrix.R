236747c67f670e41980d408e47b1eee91f4ecb6a
reetesh choudhury <reetesh.choudhury@gmail.com>
2020-10-14 10:44:29 +0530

    completed cachesolve()

--- cachematrix.R
+++ cachematrix.R
@@ -2,6 +2,7 @@
 ## functions do
 
 ## Write a short comment describing this function
+## defining list of functions for the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
 invert<-NULL
@@ -17,7 +18,16 @@ list(set = set, get = get, setInverse = setmatrix, getInverse = getmatrix)
 
 
 ## Write a short comment describing this function
-
+## To check if the inversr of the matrix is in cache else put it in cache
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  invert<-x$getmatrix()
+  if(!is.null(invert)){
+    message("displaying cached data")
+    return(invert)
+  }
+  new_matrix<-x$get()
+  invert<-solve(new_matrix, ...)
+  x$setmatrix(invert)
+  invert
 }
