6ac26ddba57c5ef84860882af9f1ea7d3a645982
reetesh choudhury <reetesh.choudhury@gmail.com>
2020-10-14 10:37:00 +0530

    completed makeCacheMatrix()

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,15 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+invert<-NULL
+set<-function(y){
+  x<<-y
+  invert<<-NULL
+}
+get <- function() x
+setmatrix <- function(solve) invert <<- solve
+getmatrix <- function() invert
+list(set = set, get = get, setInverse = setmatrix, getInverse = getmatrix)
 }
 
 
